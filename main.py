7b7171a5b6ebba97cf94d46245b914969e2646f4
Abhay Kumar Chaurasiya <55340948+chaurasiyag@users.noreply.github.com>
2022-11-19 22:46:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-from github import Github
-ac=Github('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')
-repo=ac.get_repo("chaurasiyag/minor-project-quantum-computing")
 
-path="/home/kalg/quantum_circuit.ipynb"
