c828a7f9d1a2c1b113c8e98b15b88c46f30cf9b4
chaurasiyag <abhaychaurasiya97@gmail.com>
2022-11-19 22:23:10 +0530

    Created repo

---
+++ main.py
@@ -0,0 +1,5 @@
+from github import Github
+ac=Github('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')
+repo=ac.get_repo("chaurasiyag/minor-project-quantum-computing")
+
+path="/home/kalg/quantum_circuit.ipynb"
